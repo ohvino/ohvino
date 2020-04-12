e132b03b4a2b84c0a1e8919f1baa77162c5e136a
Boris <borishoz@gmail.com>
2020-04-12 10:59:35 +0300

    iOs +/- reduced

--- lib/main.dart
+++ lib/main.dart
@@ -146,7 +146,7 @@ class PlusMinusButton extends Padding {
   PlusMinusButton( String txt, this._func) : super (
     padding: EdgeInsets.all(_edgePlus),
     child: RaisedButton(
-      padding: EdgeInsets.all(_edgePlus),
+      padding: EdgeInsets.all(_padPreset),
       shape: new RoundedRectangleBorder( side: new BorderSide(width: 3.0, color: Colors.black),
           borderRadius: new BorderRadius.circular(30.0)),
       color: Colors.blue,
@@ -162,7 +162,8 @@ class PlusMinusButton extends Padding {
     )
   );
 
-  static const double _edgePlus = 15.0;
+  static const double _edgePlus = 13.0;
+  static const double _padPreset = 15.0;
   static const double _fontPlus = 40.0;
 
   Function() _func;
@@ -356,29 +357,11 @@ class MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
